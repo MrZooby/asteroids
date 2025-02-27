2ed2ee73781583b6a259623cd01e5bd40ecc52d0
MrZooby <brandon.zuchowski91@gmail.com>
2025-02-27 16:02:50 -0500

    switched to containers in main.py

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -19,8 +24,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         time_passed = clock.tick(60)
         dt = time_passed / 1000
